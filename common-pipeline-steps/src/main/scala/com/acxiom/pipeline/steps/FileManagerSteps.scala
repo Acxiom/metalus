50e04eda5dc3f678d3559fc36a81d5118587fd2f
dafreels <dafreels@yahoo.com>
2019-05-22 14:23:26 -0400

    - Applied optimal buffer sizes to the FileManager

--- common-pipeline-steps/src/main/scala/com/acxiom/pipeline/steps/FileManagerSteps.scala
+++ common-pipeline-steps/src/main/scala/com/acxiom/pipeline/steps/FileManagerSteps.scala
@@ -20,10 +20,30 @@ object FileManagerSteps {
     "Pipeline",
     "InputOutput")
   def copy(srcFS: FileManager, srcPath: String, destFS: FileManager, destPath: String): Boolean = {
+    copy(srcFS, srcPath, destFS, destPath, FileManager.DEFAULT_BUFFER_SIZE, FileManager.DEFAULT_BUFFER_SIZE)
+  }
+
+  /**
+    * Copy the contents of the source path to the destination path. This function will call connect on both FileManagers.
+    *
+    * @param srcFS    FileManager for the source file system
+    * @param srcPath  Source path
+    * @param destFS   FileManager for the destination file system
+    * @param destPath Destination path
+    * @param inputBufferSize The size of the buffer for the input stream
+    * @param outputBufferSize The size of the buffer for the output stream
+    * @return True if the contents were copied.
+    */
+  @StepFunction("c40169a3-1e77-51ab-9e0a-3f24fb98beef",
+    "Copy source contents to destination with buffering",
+    "Copy the contents of the source path to the destination path. This function will call connect on both FileManagers.",
+    "Pipeline",
+    "InputOutput")
+  def copy(srcFS: FileManager, srcPath: String, destFS: FileManager, destPath: String, inputBufferSize: Int, outputBufferSize: Int): Boolean = {
     srcFS.connect()
     destFS.connect()
-    val inputStream = srcFS.getInputStream(srcPath)
-    val outputStream = destFS.getOutputStream(destPath)
+    val inputStream = srcFS.getInputStream(srcPath, inputBufferSize)
+    val outputStream = destFS.getOutputStream(destPath, bufferSize = outputBufferSize)
     val copied = destFS.copy(inputStream, outputStream)
     inputStream.close()
     outputStream.close()
