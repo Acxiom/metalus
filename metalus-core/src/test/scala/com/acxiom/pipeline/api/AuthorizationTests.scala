64bcdda629d232be73374a5cd7c355a418bb0414
dafreels <dafreels@yahoo.com>
2020-04-20 10:42:41 -0400

    - Moved test for SessionAuthorization to core module

---
+++ metalus-core/src/test/scala/com/acxiom/pipeline/api/AuthorizationTests.scala
@@ -0,0 +1,34 @@
+package com.acxiom.pipeline.api
+
+import java.net.URL
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, post, urlPathEqualTo}
+import org.scalatest.{BeforeAndAfterAll, FunSpec}
+
+class AuthorizationTests extends FunSpec with BeforeAndAfterAll {
+  private val HTTP_PORT = 10295
+
+  private val wireMockServer = new WireMockServer(HTTP_PORT)
+
+  override def beforeAll(): Unit = {
+    wireMockServer.start()
+  }
+
+  override def afterAll(): Unit = {
+    wireMockServer.stop()
+  }
+
+  describe("Should test SessionAuthorization") {
+    it("Should verify the SessionAuthorization") {
+      wireMockServer.addStubMapping(post(urlPathEqualTo("/api/v1/users/login"))
+        .willReturn(aResponse()
+          .withHeader("Set-Cookie", "somevalue=session_id")).build())
+
+      val sessionAuth = SessionAuthorization("test", "test", s"${wireMockServer.baseUrl()}/api/v1/users/login")
+      val connection = new URL(wireMockServer.baseUrl()).openConnection()
+      sessionAuth.authorize(connection)
+      assert(connection.getRequestProperty("Cookie") == "somevalue=session_id")
+    }
+  }
+}
