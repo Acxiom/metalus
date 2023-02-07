c1b24b5c2193ce3e3d67c42bc9155988962f022e
dafree <david.a.freels@acxiom.com>
2023-02-07 14:27:51 -0500

    updating jdbc dataref code

---
+++ metalus-core/src/main/scala/com/acxiom/metalus/sql/jdbc/JDBCDataReference.scala
@@ -0,0 +1,52 @@
+package com.acxiom.metalus.sql.jdbc
+
+import com.acxiom.metalus.PipelineContext
+import com.acxiom.metalus.sql.{QueryOperator, SqlBuildingDataReference}
+
+import java.sql.{Connection, DriverManager, ResultSet}
+import java.util.Properties
+import scala.collection.immutable.Queue
+import scala.jdk.CollectionConverters._
+import scala.util.{Failure, Success, Try}
+
+trait JDBCDataReference[T] extends SqlBuildingDataReference[T] {
+
+  override def engine: String = s"jdbc[$queryEngine]"
+  def queryEngine: String = Try(uri.split(':')(1)).toOption.getOrElse("unknown")
+
+  def uri: String
+
+  def properties: Map[String, String]
+
+  protected def createConnection(): Connection = {
+    val props = new Properties()
+    props.putAll(properties.asJava)
+    DriverManager.getConnection(uri, props)
+  }
+
+}
+
+final case class BasicJDBCDataReference(initialReference: String,
+                                        uri: String,
+                                        properties: Map[String, String],
+                                        logicalPlan: Queue[QueryOperator] = Queue(),
+                                        pipelineContext: PipelineContext)
+  extends JDBCDataReference[ResultSet] {
+
+  override def execute: ResultSet = {
+    Try(createConnection()).flatMap { connection =>
+      val stmt = connection.createStatement()
+      val exe = Try(stmt.executeQuery(toSql))
+      stmt.close()
+      connection.close()
+      exe
+    } match {
+      case Success(rs) => rs
+      case Failure(exception) => throw exception
+    }
+  }
+
+  override protected def queryOperations: QueryFunction = {
+    case qo if qo.supported => copy(logicalPlan = updateLogicalPlan(qo))
+  }
+}
