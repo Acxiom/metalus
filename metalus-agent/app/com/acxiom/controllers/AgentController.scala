a84be9b2115660dbb8e1d90b3529b0bb2ab3241f
dafree <david.a.freels@acxiom.com>
2023-04-14 13:48:28 -0400

    request scaffolding

--- metalus-agent/app/com/acxiom/controllers/AgentController.scala
+++ metalus-agent/app/com/acxiom/controllers/AgentController.scala
@@ -1,10 +1,21 @@
 package com.acxiom.controllers
 
-import play.api.mvc.{BaseController, ControllerComponents}
+import com.acxiom.utils.{AgentUtils, ApplicationRequest}
+import com.github.tototoshi.play2.json4s.Json4s
+import org.json4s.{Formats, JValue}
+import play.api.Configuration
+import play.api.mvc.{Action, BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
 
 @Singleton
-class AgentController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-
+class AgentController @Inject()(json4s: Json4s,
+                                val controllerComponents: ControllerComponents,
+                                val config: Configuration) extends MetalusAgentBaseController {
+  implicit val json4sFormats: Formats = org.json4s.DefaultFormats
+  def execute(): Action[JValue] = Action(json4s.json) { implicit request =>
+    request.extract[ApplicationRequest]{ app =>
+      Ok(AgentUtils.executeRequest(app, config))
+    }
+  }
 }
--- metalus-agent/app/com/acxiom/utils/ProcessInfo.scala
+++ metalus-agent/app/com/acxiom/utils/ProcessInfo.scala
@@ -1,3 +1,4 @@
 package com.acxiom.utils
 
-case class ProcessInfo(agentId: String, sessionId: String, processId: Long, hostName: String, command: List[String])
+final case class ProcessInfo(agentId: String, sessionId: String, processId: Long, hostName: String, command: List[String])
+  extends ApiResponse
