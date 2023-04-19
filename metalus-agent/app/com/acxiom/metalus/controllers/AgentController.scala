64901d56b7fb393ce6bd0d6b56b212d59a0735e1
dafree <david.a.freels@acxiom.com>
2023-04-19 15:32:09 -0400

    merge cleanup

--- metalus-agent/app/com/acxiom/metalus/controllers/AgentController.scala
+++ metalus-agent/app/com/acxiom/metalus/controllers/AgentController.scala
@@ -1,9 +1,8 @@
 package com.acxiom.metalus.controllers
 
-import com.acxiom.utils.{AgentUtils, ApplicationRequest}
+import com.acxiom.metalus.utils.{AgentUtils, ApplicationRequest}
 import com.github.tototoshi.play2.json4s.Json4s
 import org.json4s.{Formats, JValue}
-import play.api.Configuration
 import play.api.mvc.{Action, BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
@@ -11,12 +10,12 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class AgentController @Inject()(json4s: Json4s,
                                 val controllerComponents: ControllerComponents,
-                                val config: Configuration) extends MetalusAgentBaseController {
+                                val agentUtils: AgentUtils) extends MetalusAgentBaseController {
   implicit val json4sFormats: Formats = org.json4s.DefaultFormats
   def execute(): Action[JValue] = Action(json4s.tolerantJson) { implicit request =>
     request.extract[ApplicationRequest]{
       case app if app.application.pipelineId.isEmpty => request.parseError[ApplicationRequest]
-      case app => Ok(AgentUtils.executeRequest(app, config))
+      case app => Ok(agentUtils.executeRequest(app))
     }
   }
 }
---
+++ metalus-agent/app/com/acxiom/metalus/controllers/MetalusAgentBaseController.scala
@@ -0,0 +1,51 @@
+package com.acxiom.metalus.controllers
+
+import com.acxiom.metalus.utils.ApiError
+import org.json4s.Formats
+import org.json4s.JsonAST.JValue
+import play.api.{Logging, MarkerContext}
+import play.api.mvc.{BaseController, Request, Result}
+
+import scala.reflect.runtime.universe.TypeTag
+
+trait MetalusAgentBaseController extends BaseController with Logging {
+
+  implicit class RequestImplicits(request: Request[JValue]) {
+    /**
+     * Extracts the request body into the desired type, if possible. Returns a BadRequest otherwise.
+     *
+     * @param func A function that operates on the extracted object, and returns a Result.
+     * @tparam A The Object type to be extracted from the request body.
+     * @return A Result object returned from func, or a BadRequest result if the boy could not be extracted.
+     */
+    def extract[A](func: A => Result)
+                  (implicit formats: Formats, ev: Manifest[A], typeTag: TypeTag[A], mc: MarkerContext): Result = {
+      request.body.extract[Option[A]].map(func).getOrElse(parseError[A])
+    }
+
+    def extractOption[A](func: Option[A] => Result)(implicit formats: Formats, ev: Manifest[A]): Result = {
+      if (request.hasBody) {
+        func(request.body.extractOpt[A])
+      } else {
+        func(None)
+      }
+    }
+
+    def parseError[A](implicit ev: Manifest[A], typeTag: TypeTag[A]): Result = {
+      val members = typeTag.tpe.members.collect {
+        case m if m.isMethod && m.asMethod.isCaseAccessor => m.asMethod
+      }
+        .map(m => s"${m.name.toString}: ${m.typeSignature.toString.replaceAllLiterally("=> ", "")}")
+        .mkString(", ")
+      BadRequest(ParseError(
+        request.id.toString,
+        s"Failed to parse request body into expected object: ${ev.toString}",
+        s"${ev.runtimeClass.getSimpleName}($members)",
+        request.body
+      ))
+    }
+  }
+
+}
+
+final case class ParseError(requestId: String, message: String, expected: String, requestBody: JValue) extends ApiError
